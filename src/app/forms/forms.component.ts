66a56e48dea7cdbd55d4a323d3868768b42b17a5
AmarS <amarsingh95@gmail.com>
2023-10-09 21:26:15 +0530

    added changes in forms ts

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,20 +1,21 @@
-import { Component, OnInit,ChangeDetectionStrategy} from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
   styleUrls: ['./forms.component.css'],
-  changeDetection:ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FormsComponent implements OnInit {
 
   constructor(private fb: FormBuilder) { }
 
-  genderArr:Array<string>=['Male','Female','Others']
-  langArr:Array<{value:string,selected:boolean,touched:boolean}>=[
-    {value:'Hindi',selected:false,touched:false},
-  {value:'Marathi',selected:false,touched:false},
-  {value:'English',selected:false,touched:false}]
+  genderArr: Array<string> = ['Male', 'Female', 'Others']
+  langArr: Array<{ value: string, selected: boolean, touched: boolean }> = [
+    { value: 'Hindi', selected: false, touched: false },
+    { value: 'Marathi', selected: false, touched: false },
+    { value: 'English', selected: false, touched: false }]
+  chkIndex: number | undefined;
 
 
   //Three Level Nested dynamic form
@@ -40,21 +41,21 @@ export class FormsComponent implements OnInit {
 
   createStudent() {
 
-    let lanArr:Array<any>=[];
-    this.langArr.map((dt:any)=>{
+    let lanArr: Array<any> = [];
+    this.langArr.map((dt: any) => {
       lanArr.push(this.fb.control(dt))
-    })  
+    })
     return this.fb.group({
       firstname: ['', [Validators.required]],
       lastname: ['', [Validators.required]],
       email: ['', [Validators.required]],
-      gender:['',[Validators.required]],
-      language:[[
-      {value:'Hindi',selected:false,touched:false},
-      {value:'Marathi',selected:false,touched:false},
-      {value:'English',selected:false,touched:false}],[Validators.required]],
-      lang:this.fb.array(lanArr),
-      subject: this.fb.array([this.createSubjects()]) 
+      gender: ['', [Validators.required]],
+      language: [[
+        { value: 'Hindi', selected: false, touched: false },
+        { value: 'Marathi', selected: false, touched: false },
+        { value: 'English', selected: false, touched: false }], [Validators.required]],
+      lang: this.fb.array(lanArr),
+      subject: this.fb.array([this.createSubjects()])
     })
   }
 
@@ -86,8 +87,7 @@ export class FormsComponent implements OnInit {
     this.subjects(departmentIndex, studentIndex).push(this.createSubjects())
   }
 
-  lang(departmentIndex: number, studentIndex: number)
-  {
+  lang(departmentIndex: number, studentIndex: number) {
     return this.students(departmentIndex).at(studentIndex).get('lang') as FormArray;
   }
 
@@ -110,62 +110,79 @@ export class FormsComponent implements OnInit {
     return false
   }
 
-  hasError(FormGrp: any, formControlNameTxt: string,errName:string) {
-    if(FormGrp.get(formControlNameTxt).hasError(errName))
-    return true
-  return false
+  hasError(FormGrp: any, formControlNameTxt: string, errName: string) {
+    if (FormGrp.get(formControlNameTxt).hasError(errName))
+      return true
+    return false
   }
 
-  getFormValidity(FormGrp: any, formControlNameTxt: string,errName:string,chkType:boolean){
-    return this.validateControls(FormGrp, formControlNameTxt) && this.hasError(FormGrp, formControlNameTxt,errName)
+  getFormValidity(FormGrp: any, formControlNameTxt: string, errName: string, chkType: boolean) {
+    return this.validateControls(FormGrp, formControlNameTxt) && this.hasError(FormGrp, formControlNameTxt, errName)
   }
 
-  getChkBoxtounchedFocus(dIndex:number,studIndex:number,lanIndex:number)
-  {
-   this.lang(dIndex, studIndex)?.value?.map((dt:any,di:number)=>{
-    lanIndex===di?Object.assign(dt,{touched:true}):''
-   })
-  }
-
-
-  getChkBoxCahngeVal(dIndex:number,studIndex:number,lanIndex:number)
-  {
-    this.lang(dIndex, studIndex)?.value?.map((dt:any,di:number)=>{
-      lanIndex===di?Object.assign(dt,{selected:!dt?.selected}):''
-     })
-  }
-
-  getChkBoxtounchedControls(dIndex:number,studIndex:number)
-  {
-   return this.lang(dIndex, studIndex)?.value?.every((dt:any)=>dt?.touched)
-  }
-
-  getChkBoxValidControls(dIndex:number,studIndex:number)
-  {
-   return this.lang(dIndex, studIndex)?.value?.every((dt:any)=>!dt?.selected)
+  getChkBoxtounchedFocus(dIndex: number, studIndex: number, lanIndex: number) {
+    this.lang(dIndex, studIndex)?.value?.map((dt: any, di: number) => {
+      lanIndex === di ? Object.assign(dt, { touched: true }) : ''
+    })
   }
 
 
-  setChkValidatorsTouchedSave()
-  {
-    this.department.controls?.map((dt:any,di:number)=>{
-      dt?.controls?.students?.controls?.map((dts:any,dtsI:number)=>{
-         dts?.controls?.lang.value?.map((dl:any,dli:number)=>{
-          this.getChkBoxtounchedFocus(di,dtsI,dli);
-         })
+  getChkBoxCahngeVal(dIndex: number, studIndex: number, lanIndex: number) {
+    this.lang(dIndex, studIndex)?.value?.map((dt: any, di: number) => {
+      lanIndex === di ? Object.assign(dt, { selected: !dt?.selected }) : ''
+    })
+  }
+
+  getChkBoxtounchedControls(dIndex: number, studIndex: number) {
+    return this.lang(dIndex, studIndex)?.value?.every((dt: any) => dt?.touched)
+  }
+
+  getChkBoxValidControls(dIndex: number, studIndex: number) {
+    return this.lang(dIndex, studIndex)?.value?.every((dt: any) => !dt?.selected)
+  }
+
+  setChkValidatorsTouchedSave() {
+    this.department.controls?.map((dt: any, di: number) => {
+      dt?.controls?.students?.controls?.map((dts: any, dtsI: number) => {
+        dts?.controls?.lang.value?.map((dl: any, dli: number) => {
+          this.getChkBoxtounchedFocus(di, dtsI, dli);
+        })
       })
     })
-    
-    // console.log(this.department.controls)
   }
-  
+
+
+
+  ///My code
+  getChkBoxtounchedControlsC(FormGrp: any, formControlName: string) {
+    return FormGrp.get(formControlName)?.value?.every((dt: any) => dt?.touched)
+  }
+
+  getChkBoxValidControlsC(FormGrp: any, formControlName: string) {
+    return FormGrp.get(formControlName)?.value?.every((dt: any) => !dt?.selected)
+  }
+
+
+  setChkValidatorsTouchedSaveC() {
+    this.department.controls?.map((dt: any, di: number) => {
+      dt?.controls?.students?.controls?.map((dts: any, dtsI: number) => {
+        let arr: Array<any> = dts?.get('language')?.value;
+        arr?.map((dd: any) => {
+          Object.assign(dd, { touched: true })
+        })
+        dts?.get('language').setValue(arr);
+      })
+    })
+  }
+
   saveUniversity() {
     if (this.university.invalid) {
       this.university.markAllAsTouched();
       this.setChkValidatorsTouchedSave();
+      this.setChkValidatorsTouchedSaveC();
     } else {
     }
   }
-  
+
 
 }
