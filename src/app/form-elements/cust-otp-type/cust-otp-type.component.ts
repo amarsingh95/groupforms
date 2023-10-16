e07caa02784420040ad9f777b157c189e8ac9281
AmarS <amarsingh95@gmail.com>
2023-10-16 13:22:13 +0530

    build cust-otp dyanamic

--- src/app/form-elements/cust-otp-type/cust-otp-type.component.ts
+++ src/app/form-elements/cust-otp-type/cust-otp-type.component.ts
@@ -1,5 +1,10 @@
 import { Component, ElementRef, OnInit,ViewChild} from '@angular/core';
 
+interface otpValType{
+ value:string,
+ text:string, 
+}
+
 @Component({
   selector: 'app-cust-otp-type',
   templateUrl: './cust-otp-type.component.html',
@@ -8,49 +13,38 @@ import { Component, ElementRef, OnInit,ViewChild} from '@angular/core';
 export class CustOtpTypeComponent implements OnInit {
 
   constructor() { }
-@ViewChild('otp1',{ static: false })otpf:ElementRef<HTMLInputElement>={} as ElementRef;
-@ViewChild('otp2',{ static: false })otps:ElementRef<HTMLInputElement>={} as ElementRef;
-@ViewChild('otp3',{ static: false })otpt:ElementRef<HTMLInputElement>={} as ElementRef;
-@ViewChild('otp4',{ static: false })otpfr:ElementRef<HTMLInputElement>={} as ElementRef;
+otpArr:Array<otpValType>=[
+  {value:'1',text:'1'},
+  {value:'2',text:'2'},
+  {value:'3',text:'3'},
+]
 
   ngOnInit(): void {
+   
   }
-
-  changeOpt1()
-  {
-
-    if(!this.checkInputNumber(this.otpf.nativeElement?.value))
-    {
-      this.otps.nativeElement.focus();
-    }
-  }
-
-  
-
-  changeOpt2()
-  {
-    if(!this.checkInputNumber(this.otps.nativeElement?.value))
-    {
-      this.otpt.nativeElement.focus();
-    }
-    
-  }
-
-  changeOpt3()
-  {
-    if(!this.checkInputNumber(this.otpt.nativeElement?.value))
-    {
-      this.otpfr.nativeElement.focus();
-    }
-  }
-
-  changeOpt4()
-  {
-  }
-
   checkInputNumber(value:string):boolean
   {
     return isNaN(parseInt(value.trim()));
+  }
+
+  changeKeyUp(event:KeyboardEvent,child:HTMLElement|HTMLInputElement,optIndex:number,inputMaxLength:number)
+  {
+    let childElement:HTMLInputElement=child as HTMLInputElement;
+    let focusPreveousElementBackSpace:HTMLElement=child.previousElementSibling as HTMLHtmlElement;
+    let focusNextElement:HTMLElement=child.nextElementSibling as HTMLHtmlElement;
+
+    if(event.key==='Backspace' && optIndex>0)
+    {
+      focusPreveousElementBackSpace.focus();
+    }
+    if(!this.checkInputNumber(childElement?.value))
+    {
+      if(optIndex!==(inputMaxLength-1))
+      {
+        focusNextElement.focus()
+      }
+      
+    }
   }
 
 }
