6f216917ea9e308f3e11186efbcb2b3e33b11e03
AmarS <amarsingh95@gmail.com>
2023-11-01 12:51:49 +0530

    added model

---
+++ src/app/models/form.model.ts
@@ -0,0 +1,50 @@
+export type multiSelectType={
+    value: string,
+    text: string,
+    selected: boolean
+  }
+  
+  
+  export type University=
+  {
+    uname:string,
+    department:Department[]
+  }
+  export type Department={
+    dname:string,
+    students:Student[]
+  }
+  
+  export type Student={
+    firstname:string,
+    lastname:string,
+    email:string,
+    gender:string,
+    country:string,
+    topics:string,
+    language:Language[],
+    subject:Subject[]
+    otp:otpValType[]
+  }
+  
+  export type Subject ={
+    subname:string 
+  }
+  
+  export type Language={
+      value:string,
+      selected:boolean,
+      touched:boolean
+  }
+  
+  export type otpValType={
+    value:string,
+    text:string,
+    touched:boolean 
+   }
+
+   export type valType={
+    value: string,
+    selected: boolean,
+    touched: boolean
+  }
