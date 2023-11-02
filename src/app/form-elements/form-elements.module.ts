6e0e64a09822617e9a8fb2f33cd7fce6e7d0b5b5
AmarS <amarsingh95@gmail.com>
2023-11-02 15:03:08 +0530

    added changes in form module

--- src/app/form-elements/form-elements.module.ts
+++ src/app/form-elements/form-elements.module.ts
@@ -8,6 +8,7 @@ import { CustDropdownComponent } from './cust-dropdown/cust-dropdown.component';
 import { CustMultiSelectFilterComponent } from './cust-multi-select-filter/cust-multi-select-filter.component';
 import { CustOtpTypeComponent } from './cust-otp-type/cust-otp-type.component';
 import { OnlynumberDirective } from '../onlynumber.directive';
+import { CustFileComponent } from './cust-file/cust-file.component';
 
 
 
@@ -19,13 +20,14 @@ import { OnlynumberDirective } from '../onlynumber.directive';
     CustDropdownComponent,
     CustMultiSelectFilterComponent,
     CustOtpTypeComponent,
-    OnlynumberDirective
+    OnlynumberDirective,
+    CustFileComponent
   ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
     FormsModule
   ],
-  exports:[CustTextComponent,CustRadioComponent,CustCheckboxComponent,CustDropdownComponent,CustMultiSelectFilterComponent,CustOtpTypeComponent]
+  exports:[CustTextComponent,CustRadioComponent,CustCheckboxComponent,CustDropdownComponent,CustMultiSelectFilterComponent,CustOtpTypeComponent,CustFileComponent]
 })
 export class FormElementsModule { }
