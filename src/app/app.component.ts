43216aa45ee57ef56e8660abf9f762875a26be14
AmarS <amarsingh95@gmail.com>
2023-11-07 11:58:38 +0530

    added global loader in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { ServicesService } from './services.service';
+import { delay } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'nested-forms';
+  load:boolean=true;
+  constructor(private service:ServicesService){}
+  
+  ngOnInit(): void {
+   this.loading();   
+  }
+
+  loading()
+  {
+    this.service.loadingSpinner.pipe(delay(0)).subscribe((loadingValue)=>{
+      this.load=loadingValue;
+    })
+  }
 }
